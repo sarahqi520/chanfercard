ae6f1944ef6f88a6376b297a3e804fa148164542
sarahqi520 <sarahqi520@gmail.com>
2026-06-23 13:37:57 +0800

    feat: add Google Tag Manager (GTM-N8CX7M0W) for chanfercard.com

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -216,6 +216,17 @@ export default async function LangLayout({ children, params }: Props) {
 
   return (
     <>
+      {/* Google Tag Manager */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N8CX7M0W');`,
+        }}
+      />
+
       {/* Structured data - Organization */}
       <script
         type="application/ld+json"
